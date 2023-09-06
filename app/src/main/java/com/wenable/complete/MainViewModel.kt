8bcfbbc085f49c77fab864526b90aec15d747601
KabeerSohail <sohail.mohammad@wenable.com>
2023-09-06 11:22:09 +0530

    1. added two way data binding for direct communication between activity and view model 2. able to call a function with parameter from edit text on click of a button

--- app/src/main/java/com/wenable/complete/MainViewModel.kt
+++ app/src/main/java/com/wenable/complete/MainViewModel.kt
@@ -1,9 +1,15 @@
 package com.wenable.complete
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
 
-    var name: String = "Remote Configuration of Certificate Authenticated WIFI"
+    var name: String = ""
+
+    fun setNameX(name: String) {
+        this.name = name
+        Log.d("@@@##$%", name)
+    }
 
 }
