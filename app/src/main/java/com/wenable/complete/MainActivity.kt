a7bf57eb3b3ad88b65ef98657f5695acfab864c2
KabeerSohail <sohail.mohammad@wenable.com>
2023-09-06 12:11:01 +0530

    basic commit

--- app/src/main/java/com/wenable/complete/MainActivity.kt
+++ app/src/main/java/com/wenable/complete/MainActivity.kt
@@ -9,12 +9,10 @@ import com.wenable.complete.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.lifecycleOwner = this
-        binding.viewModel = mainViewModel
+
     }
 }
--- app/src/main/java/com/wenable/complete/MainViewModel.kt
+++ app/src/main/java/com/wenable/complete/MainViewModel.kt
@@ -1,23 +1,7 @@
 package com.wenable.complete
 
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
-
-    private var _name: MutableLiveData<String> = MutableLiveData("Remote Configuration of Certificate Authenticated WIFI")
-    var name: LiveData<String> = _name
-    var x: Int = 0
-
-    fun change() {
-        x++
-        _name.value = x.toString()
-    }
-
-    fun changed(text: String) {
-        Log.d("##@$%@%@", text)
-    }
 
 }
