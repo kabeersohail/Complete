fa984fd07391d73307ab19a001b9c4ae714fa12f
KabeerSohail <sohail.mohammad@wenable.com>
2023-09-06 12:24:50 +0530

    live data is working

--- app/src/main/java/com/wenable/complete/MainActivity.kt
+++ app/src/main/java/com/wenable/complete/MainActivity.kt
@@ -2,6 +2,7 @@ package com.wenable.complete
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.wenable.complete.databinding.ActivityMainBinding
@@ -9,10 +10,17 @@ import com.wenable.complete.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        viewModel.liveData.observe(this) {
+            Log.d("#$$^$^$", it)
+        }
     }
 }
--- app/src/main/java/com/wenable/complete/MainViewModel.kt
+++ app/src/main/java/com/wenable/complete/MainViewModel.kt
@@ -1,7 +1,16 @@
 package com.wenable.complete
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
+
+    private val _mutableLiveData: MutableLiveData<String> = MutableLiveData()
+    val liveData: LiveData<String> = _mutableLiveData
+
+    fun postNewValue() {
+        _mutableLiveData.postValue("SOS")
+    }
 
 }
