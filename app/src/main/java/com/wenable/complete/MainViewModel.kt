3bbe9c70433f67cbaf53dd1c80dc74cd7d40097d
KabeerSohail <sohail.mohammad@wenable.com>
2023-09-06 14:30:00 +0530

    basic commit

--- app/src/main/java/com/wenable/complete/MainViewModel.kt
+++ app/src/main/java/com/wenable/complete/MainViewModel.kt
@@ -1,24 +1,6 @@
 package com.wenable.complete
 
-import android.text.Editable
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
-
-    private var _name: MutableLiveData<String> = MutableLiveData("Remote Configuration of Certificate Authenticated WIFI")
-    var name: LiveData<String> = _name
-    var x: Int = 0
-
-    fun change(name: String) {
-        x++
-        _name.value = x.toString()
-    }
-
-    fun changed(text: Editable) {
-        Log.d("##@$%@%@", text.toString())
-    }
-
 }
