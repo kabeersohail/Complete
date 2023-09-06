94d06d8ae8d1e56df2dc2bbda3baed0a3ed48d9a
KabeerSohail <sohail.mohammad@wenable.com>
2023-09-06 11:10:26 +0530

    1. created a view model variable in xml file 2. initiated view model in MainActivity 3. populated text view directly from view model without intervention of MainActivity

--- app/src/main/java/com/wenable/complete/MainActivity.kt
+++ app/src/main/java/com/wenable/complete/MainActivity.kt
@@ -2,15 +2,18 @@ package com.wenable.complete
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.wenable.complete.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = mainViewModel
     }
 }
---
+++ app/src/main/java/com/wenable/complete/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.wenable.complete
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    var name: String = "Remote Configuration of Certificate Authenticated WIFI"
+
+}
